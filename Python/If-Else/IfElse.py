916212db83d5bb6ee9c2b1b9a26cab54b8596dcc
Arief Purnama Muharram <ariefpurnamamuharram@gmail.com>
2020-04-20 02:47:24 +0700

    First commit: Python challenge.

---
+++ Python/If-Else/IfElse.py
@@ -0,0 +1,18 @@
+# If-Else
+
+
+def is_weird(n):
+    if n % 2 == 0:
+        if n in range(2, 5):
+            return "Not Weird"
+        elif n in range(6, 21):
+            return "Weird"
+        else:
+            return "Not Weird"
+    else:
+        return "Weird"
+
+
+if __name__ == '__main__':
+    a = int(input())
+    print(is_weird(a))
